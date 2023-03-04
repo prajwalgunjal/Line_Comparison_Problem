0f16806e4fcdd234d7204a71127931539aa23052
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-04 10:34:52 +0530

    first commit

---
+++ LineComparison.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+import java.util.Scanner;
+public class LineComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Line Comparison program.");
+
+    }
+}
