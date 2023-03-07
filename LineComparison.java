967c1c07250184a2ff9642317f2d63c378f169df
prajwalgunjal <prajwalgunjal8@gmail.com>
2023-03-08 00:22:37 +0530

    uc4:Added

--- LineComparison.java
+++ LineComparison.java
@@ -1,41 +1,44 @@
 package com.bridgelabz;
 import java.util.Scanner;
 public class LineComparison {
-    public static void main(String[] args) {
+    int x1,x2,y1,y2,x3,x4,y3,y4;
+    int lengthofline1,lengthofline2;
+    public void LengthOfLine1(){
         Scanner sc = new Scanner(System.in);
-
-        // length of line 1
         System.out.println("Welcome to Line Comparison program.");
         System.out.println("Enter the value of x1");
-        int x1 = sc.nextInt();
+        x1 = sc.nextInt();
         System.out.println("Enter the value of X2");
-        int x2 = sc.nextInt();
+        x2 = sc.nextInt();
         System.out.println("ENter the value of y1");
-        int y1 = sc.nextInt();
+        y1 = sc.nextInt();
         System.out.println("Enter the value of y2:");
-        int y2 = sc.nextInt();
-        double lengthofline1 = Math.sqrt((x2-x1)^2+(y2-y1)^2);
+        y2 = sc.nextInt();
+        lengthofline1 = (int)Math.sqrt((x2-x1)^2+(y2-y1)^2);
         System.out.println("Length of Line1 is : "+lengthofline1);
-
-        //length of line 2
+    }
+    public void LengthOfLine2(){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of x3:");
-        int x3 = sc.nextInt();
+        x3 = sc.nextInt();
         System.out.println("Enter the value of y3:");
-        int y3 = sc.nextInt();
+        y3 = sc.nextInt();
         System.out.println("Enter the value of x4:");
-        int x4 =sc.nextInt();
+        x4 =sc.nextInt();
         System.out.println("Enter the value of y4:");
-        int y4 = sc.nextInt();
-        double lengthofline2 = Math.sqrt((x3^2-x4^2)+(y3^2-y4^2));
+        y4 = sc.nextInt();
+        int lengthofline2 = (int)Math.sqrt((x3^2-x4^2)+(y3^2-y4^2));
         System.out.println("length of line 2 is : "+lengthofline2);
-        if(lengthofline1==lengthofline2)
-        {
+    }
+    public void Checkequal() {
+        String str1 = Double.toString(lengthofline1);
+        String str2 = Double.toString(lengthofline1);
+
+        if (str1.equals(str2) == true)
             System.out.println("Both lines are equal");
-        }
         else
-        {
-            System.out.println("lines are not equal");
-        }
+            System.out.println("Not both lines are equal");
+
         if(lengthofline1<lengthofline2)
         {
             System.out.println("line 2 is greater than line 1");
@@ -43,5 +46,13 @@ public class LineComparison {
         else {
             System.out.println("line 1 is greater than line 2");
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LineComparison lc = new LineComparison();
+        lc.LengthOfLine1();
+        lc.LengthOfLine2();
+        lc.Checkequal();
     }
 }
